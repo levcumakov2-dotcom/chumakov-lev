094450bb89ca1f5343920665df7f44130ce9050a
levcumakov2-dotcom <levcumakov2@gmail.com>
2026-06-17 11:20:31 +0700

    Delete blog directory

--- blog/includes/footer.php
+++
@@ -1,9 +0,0 @@
-    </main>
-    <footer class="footer">
-        <div class="container">
-            <p>&copy; <?= date('Y') ?> Мой Блог. Все права защищены.</p>
-        </div>
-    </footer>
-    <script src="assets/js/script.js"></script>
-</body>
-</html>
